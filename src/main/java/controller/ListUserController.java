0d1ab1d511320419a969853b42c220223d4af55f
leekm0310 <leekm310@gmail.com>
2025-03-05 18:59:14 +0900

    feat: ListUserController 생성
    - 로그인 시 유저 리스트 생성 로직을 처리
    - isLogin 메소드로 로그인 여부 쿠키로 판별

---
+++ src/main/java/controller/ListUserController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequest;
+import util.HttpRequestUtils;
+import util.HttpResponse;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ListUserController implements Controller {
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        //로그인 되어 있는지 확인 - 쿠키로
+        if (!isLogin(request.getHeader("Cookie"))) {
+            response.redirect("/user/login.html");
+            return;
+        }
+        // 로그인 되어 있으면 로그인 유저 리스트 보여줌
+        Collection<User> userList = DataBase.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table><tbody>");
+        for (User user : userList) {
+            sb.append("</th>");
+            sb.append("<td>" + user.getUserId() + "</td>");
+            sb.append("<td>" + user.getName() + "</td>");
+            sb.append("<td>" + user.getEmail() + "</td>");
+        }
+        sb.append("</tbody></table>");
+        response.forwardBody(sb.toString());
+    }
+
+    boolean isLogin(String line) {
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(line);
+        String value = cookies.get("logined");
+        if (value == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(value);
+    }
+}
