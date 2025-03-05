8b36e019f0f04506b0fb74927a76b35a672f51b7
leekm0310 <leekm310@gmail.com>
2025-03-05 18:57:00 +0900

    feat: Controller 인터페이스 및 createUserController 생성
    - Controller 인터페이스를 implement하는 CreateUserController 생성
    - CreateUserController의 service에서 회원가입 로직 처리

---
+++ src/main/java/controller/CreateUserController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import db.DataBase;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequest;
+import util.HttpResponse;
+
+public class CreateUserController implements Controller{
+
+    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {
+        //TODO: 유저 빌더 패턴 적용해보기
+        log.debug("[called] : UserCreateController");
+        User user = new User(request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("mail"));
+        DataBase.addUser(user);
+        log.debug("[user] : {}", user);
+
+        response.redirect("/index.html");
+    }
+}
